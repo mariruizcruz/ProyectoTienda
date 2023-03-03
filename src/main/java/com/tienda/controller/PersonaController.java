b28622e16dd14ab982104a1306592b54dcf89400
MariRuizC <105657008+MariRuizC@users.noreply.github.com>
2023-03-02 20:06:49 -0600

    se puede añadir,editar y eliminar en la base de datos

--- src/main/java/com/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -45,6 +46,12 @@ public class PersonaController {
         return "crear";
     }
     
+    @GetMapping("/delete/{id}")
+    public String eliminarPersona(@PathVariable("id")Long idPersona){
+        personaService.delete(idPersona);
+        return "redirect:/persona";
+    }
+    
     @PostMapping("/save")
     public String guardarPersona(@ModelAttribute Persona persona){
         personaService.savePersona(persona);
@@ -52,4 +59,13 @@ public class PersonaController {
     }
     
     
+    @GetMapping("/editPersona/{id}")
+    
+    public String editarPersona(@PathVariable("id")Long idPersona,Model model){
+        Persona persona = personaService.getPersonaById(idPersona);
+        List<Pais>listaPaises = paisService.listCountry();
+        model.addAttribute("persona", persona);
+        model.addAttribute("paises",listaPaises);
+        return "crear";
+    }
 }
